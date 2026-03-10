4186709230472f13b1b80a6a783d4e694e48ba89
MonoSama21 <yrvin21pachas2004@gmail.com>
2026-03-10 14:42:46 -0500

    FrontEnd - V.24 Viendo si con esto arreglo o malogro - Yrvin Pachas

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-import postcssOklabFunction from '@csstools/postcss-oklab-function';
-
 export default {
-    plugins: [
-        postcssOklabFunction({ subFeatures: { displayP3: false } })
-    ]
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
